cabae9cfccae2c6ef98843c1748333fc632dd916
Shruti <shrutik0219@gmail.com>
2020-07-12 17:00:35 +0530

    Changed Char to text Field

--- articles/models.py
+++ articles/models.py
@@ -5,7 +5,7 @@ class all_info(models.Model):
     email = models.CharField(max_length=45,default="")
     author = models.CharField(max_length=20,default="Anonymous",null=True)
     title = models.CharField(max_length=50)
-    content = models.CharField(max_length=100000000000,default="Nothing Here...")
+    content = models.TextField(default="Nothing here...")
     date = models.DateField()
     image = models.ImageField(upload_to="articles/images", default="")
 
